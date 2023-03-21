b6e1b1a579b4ad33cbe6d7f89f6c57a2c9b5128d
NelsonJfsg <nelin09001@gmail.com>
2023-03-21 09:19:04 -0700

    init branch

---
+++ src/Common/components/TextFieldStyled.tsx
@@ -0,0 +1,19 @@
+import { TextField } from "@mui/material"
+
+interface Props {
+
+  labelText : string
+
+}
+
+
+export const TextFieldStyled = ({labelText} : Props) => {
+  return (
+    
+    <TextField
+    
+    
+    
+    />
+  )
+}
